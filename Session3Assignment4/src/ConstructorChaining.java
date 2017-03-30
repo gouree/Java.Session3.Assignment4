66aff99a318b4c87a4178c2a3b4c28ee93591e52
gouree <goure.deshpande@gmail.com>
2017-03-30 13:01:52 +0530

    Add files via upload

--- Session3Assignment4/src/ConstructorChaining.java
+++ Session3Assignment4/src/ConstructorChaining.java
@@ -3,25 +3,28 @@ public class ConstructorChaining
 {
 	public static void main(String args[])
 	{
-		Trial a = new Trial();
-		Trial b= new Trial(10);
-		Trial c= new Trial("hello","acadgild");
+		Trial a = new Trial();	//default constructor call
+		Trial b= new Trial(10);	//invokes parameterized constructor
+		Trial c= new Trial("hello","acadgild");	//invokes parameterized constructor
 	}
 }
 
 class Trial
 {
+	//default constructor
 	public Trial()
 	{
 		System.out.println("You are in default constructor");
 	}
 	
+	//parameterized constructor with one argument
 	public Trial( int a)
 	{
 		System.out.println("You are in parameterized constructor");
 		System.out.println("Value of the object parameter = " +a);
 	}
 	
+	//parameterized constructor with two arguments
 	public Trial( String a, String b)
 	{
 		System.out.println("You are in parameterized constructor");
